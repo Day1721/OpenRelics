6ffa3378e5f7003319ff469b5893bb9c696e216d
Day1721 <puzynamarek97@gmail.com>
2017-01-31 15:00:55 +0100

    Switch to SQL CE

--- DatabaseFiller/DatabaseFiller/DbClasses/RelicsDbContext.cs
+++ DatabaseFiller/DatabaseFiller/DbClasses/RelicsDbContext.cs
@@ -9,6 +9,16 @@ namespace DatabaseFiller.DbClasses
 {
     class RelicsDbContext : DbContext
     {
+        private class DbInitializer : DropCreateDatabaseAlways<RelicsDbContext>
+        { }
+
+        static RelicsDbContext()
+        {
+            Database.SetInitializer<RelicsDbContext>(new DbInitializer());
+            using (var db = new RelicsDbContext())
+                db.Database.Initialize(false);
+        }
+
         public DbSet<Relic> Relics { get; set; }
 
         public DbSet<Connection> Connections { get; set; }
--- OpenRelicsWebApp/OpenRelicsWebApp/Models/RelicsDbContext.cs
+++ OpenRelicsWebApp/OpenRelicsWebApp/Models/RelicsDbContext.cs
@@ -4,6 +4,21 @@ namespace OpenRelicsWebApp.Models
 {
     class RelicsDbContext : DbContext
     {
+        private class DbInitializer : IDatabaseInitializer<RelicsDbContext>
+        {
+            public void InitializeDatabase(RelicsDbContext context)
+            {
+                
+            }
+        }
+
+        static RelicsDbContext()
+        {
+            Database.SetInitializer(new DbInitializer());
+            using (var db = new RelicsDbContext())
+                db.Database.Initialize(false);
+        }
+
         public DbSet<Relic> Relics { get; set; }
 
         public DbSet<Connection> Connections { get; set; }
